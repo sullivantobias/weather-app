4463eccb10c1b2dcb32a1652185d4c2d10817f3a
sullivantobias <sullivantobias@users.noreply.github.com>
2022-03-20 16:10:27 +0000

    deploy: 328970c8347f5997052ad2eff43fc7069e4988bb

---
+++ precache-manifest.4e8fc4c3e1200a57e22579d555a4ab28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b137d0fb05ea8cfc7cc584eff8a5b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "503fe78338dd8d3a3053",
+    "url": "./static/css/main.543366c2.chunk.css"
+  },
+  {
+    "revision": "fe16f60dd83f5b5cc120",
+    "url": "./static/js/2.2b8be5d5.chunk.js"
+  },
+  {
+    "revision": "3ce6d3ae3f5d1813807c7c9761a87fb3",
+    "url": "./static/js/2.2b8be5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503fe78338dd8d3a3053",
+    "url": "./static/js/main.8c501cd2.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
